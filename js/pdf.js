56304b37b7e1673af197b088444a7de96357122e
nurlailatulhasanah1975-collab <nurlailatul.hasanah1975@gmail.com>
2026-07-28 00:06:07 +0800

    Update pdf.js

--- js/pdf.js
+++ js/pdf.js
@@ -832,8 +832,8 @@ doc.text(
 
 const halfRow = TABLE.rowHeight / 2;
 
-// Geser seluruh grid tabel turun 1 mm
-const GRID_OFFSET_Y = 1;
+// Geser seluruh grid tabel turun 2 mm
+const GRID_OFFSET_Y = 2;
 
 // Tambah panjang garis vertikal 1 mm ke atas
 const VERTICAL_UP = 1;
