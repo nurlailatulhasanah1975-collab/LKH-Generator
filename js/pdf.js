b290bf4ec55b1a7fac7defa4e081c38c8d378772
nurlailatulhasanah1975-collab <nurlailatul.hasanah1975@gmail.com>
2026-07-29 06:01:00 +0800

    Update pdf.js

--- js/pdf.js
+++ js/pdf.js
@@ -459,31 +459,31 @@ const TABLE = {
     // definisi kolom
     columns: [
 
-        { key:"no",      title:"No",                 weight:3 },
+        { key:"no",      title:"No",                 weight:24 },
     
-        { key:"tanggal", title:"Hari / Tanggal",     weight:15 },
+        { key:"tanggal", title:"Hari / Tanggal",     weight:75 },
     
-        { key:"jam",     title:"Jam Ke-",            weight:8 },
+        { key:"jam",     title:"Jam Ke-",            weight:56 },
     
-        { key:"kelas",   title:"Kelas / Ekskul",     weight:6 },
+        { key:"kelas",   title:"Kelas / Ekskul",     weight:60 },
     
-        { key:"mapel",   title:"Mata Pelajaran",     weight:11 },
+        { key:"mapel",   title:"Mata Pelajaran",     weight:110 },
     
-        { key:"kikd",    title:"No KI/KD",           weight:5 },
+        { key:"kikd",    title:"No KI/KD",           weight:50 },
     
-        { key:"materi",  title:"Kegiatan / Materi",  weight:38 },
+        { key:"materi",  title:"Kegiatan / Materi",  weight:414 },
     
-        { key:"hasil",   title:"Hasil",              weight:8 },
+        { key:"hasil",   title:"Hasil",              weight:80 },
     
-        { key:"vol",     title:"Vol",                weight:4 },
+        { key:"vol",     title:"Vol",                weight:50 },
     
-        { key:"s",       title:"S",                  weight:2 },
+        { key:"s",       title:"S",                  weight:30 },
     
-        { key:"i",       title:"I",                  weight:2 },
+        { key:"i",       title:"I",                  weight:30 },
     
-        { key:"a",       title:"A",                  weight:2 },
+        { key:"a",       title:"A",                  weight:30 },
     
-        { key:"ket",     title:"Ket",                weight:6 }
+        { key:"ket",     title:"Ket",                weight:60 }
     
     ]
 
